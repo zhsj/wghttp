581bd26784aa6bb6baec9c1937c355019f972c64
Shengjing Zhu <i@zhsj.me>
2024-05-15 20:51:51 +0800

    chore: bump wireguard-go
    Closes: #12, Closes: #14

--- conn.go
+++ conn.go
@@ -30,10 +30,12 @@ func (c *connBind) Open(port uint16) ([]conn.ReceiveFunc, uint16, error) {
 	newFNs := make([]conn.ReceiveFunc, 0, len(fns))
 	for i := range fns {
 		f := fns[i]
-		newFNs = append(newFNs, func(b []byte) (n int, ep conn.Endpoint, err error) {
-			n, ep, err = f(b)
-			if len(b) > 4 {
-				copy(b[1:4], []byte{0, 0, 0})
+		newFNs = append(newFNs, func(packets [][]byte, sizes []int, eps []conn.Endpoint) (n int, err error) {
+			n, err = f(packets, sizes, eps)
+			for i := range packets {
+				if len(packets[i]) > 4 {
+					copy(packets[i][1:4], []byte{0, 0, 0})
+				}
 			}
 			return
 		})
@@ -41,15 +43,21 @@ func (c *connBind) Open(port uint16) ([]conn.ReceiveFunc, uint16, error) {
 	return newFNs, actualPort, err
 }
 
+func (c *connBind) BatchSize() int {
+	return c.defaultBind.BatchSize()
+}
+
 func (c *connBind) Close() error { return c.defaultBind.Close() }
 
 func (c *connBind) SetMark(mark uint32) error { return c.defaultBind.SetMark(mark) }
 
-func (c *connBind) Send(b []byte, ep conn.Endpoint) error {
-	if len(b) > 4 {
-		copy(b[1:4], c.clientID)
+func (c *connBind) Send(bufs [][]byte, ep conn.Endpoint) error {
+	for i := range bufs {
+		if len(bufs[i]) > 4 {
+			copy(bufs[i][1:4], c.clientID)
+		}
 	}
-	return c.defaultBind.Send(b, ep)
+	return c.defaultBind.Send(bufs, ep)
 }
 
 func (c *connBind) ParseEndpoint(s string) (conn.Endpoint, error) {
--- go.mod
+++ go.mod
@@ -4,15 +4,15 @@ go 1.18
 
 require (
 	github.com/jessevdk/go-flags v1.5.0
-	golang.zx2c4.com/wireguard v0.0.0-20230209153558-1e2c3e5a3c14
+	golang.zx2c4.com/wireguard v0.0.0-20231211153847-12269c276173
 )
 
 require (
 	github.com/google/btree v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	golang.zx2c4.com/wintun v0.0.0-20211104114900-415007cec224 // indirect
-	gvisor.dev/gvisor v0.0.0-20221203005347-703fd9b7fbc0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
+	gvisor.dev/gvisor v0.0.0-20230927004350-cbd86285d259 // indirect
 )
